18eb9a7743831f20107fdb204a4e93536e9fa1dc
Tyler Ingram <tylerphx@gmail.com>
2021-01-25 17:26:13 -0600

    4-4 Average Test Grades mini tutorial, with exception handler and clear button.

---
+++ AvgTestGrades4-4ExceptionHandling/AvgTestGrades4-4ExceptionHandling/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AvgTestGrades4_4ExceptionHandling
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void outputDescLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double averageGrade;
+                //Calculates the average grade from the 3 test scores
+                averageGrade = (double.Parse(test1TextBox.Text) + double.Parse(test2TextBox.Text) + double.Parse(test3TextBox.Text)) / 3.0;
+                averageLabel.Text = averageGrade.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            //clear's the text
+            test1TextBox.Text = "";
+            test2TextBox.Text = "";
+            test3TextBox.Text = "";
+            averageLabel.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
